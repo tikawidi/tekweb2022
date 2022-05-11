8b55e442ae17435cca71b21a68cfda7047d12fd8
tikawidi <widi2000016007@webmail.uad.ac.id>
2022-05-11 20:51:56 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+Vue.createApp({
+    data() {
+      return {
+        message: "Hello Vue!",
+        header: {},
+        articles: []
+      };
+    },
+    methods:{    
+        getHeaderData()
+        {
+          axios
+            .get(
+              "https://raw.githubusercontent.com/tikawidi/tekweb2022/axios/axious.json"
+            )
+            .then((res) => {              
+              this.header = res.data;
+              this.getArticles();
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        },
+        getArticles()
+        {
+            axios
+            .get(
+              "https://raw.githubusercontent.com/tikawidi/tekweb2022/artikeljson/artikel.json"
+            )
+            .then((res) => {              
+              this.articles = res.data;
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+    },
+    beforeMount() {
+        this.getHeaderData()
+    }
+  }).mount("#app");
